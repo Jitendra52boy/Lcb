83b8a2b121e73c99403e28154f4f3979a95fcf52
Spparow <vbot34615@gmail.com>
2025-04-18 20:09:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_ID = int(getenv("API_ID", "29080362"))
 API_HASH = getenv("API_HASH", "2af932be312ce9c4e4ecb84bce09109e")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "7968208807:AAG7tDuqlp4OB9RrGiUgjI1saggOVE3lI0Y")
+BOT_TOKEN = getenv("BOT_TOKEN", "7620897033:AAHvcnzvSN_qYDtYIgdpvqDfET00GdTdSvA")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://musicbotxd:musicbotxd@cluster0.6thyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
